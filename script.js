ca0a41c0339b80215ebb2e4ae75b6cfea5398c12
mastertukan <60137307+mastertukan@users.noreply.github.com>
2022-10-08 06:45:59 +0200

    Add files via upload
    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+function currentDay() {
+    const d = new Date();
+    let day = d.getDay();
+
+    let identifier = "day-" + day;
+    document.getElementById(identifier).style.backgroundColor = "var(--cyan)";
+}
+
+function changeColorOnHover(index) {
+    const d = new Date();
+    let day = d.getDay();
+
+    let identifier = "day-" + index;
+
+    if (index == day) {
+        document.getElementById(identifier).style.backgroundColor = "hsl(187, 49%, 80%)";
+    } else {
+        document.getElementById(identifier).style.backgroundColor = "hsl(9, 100%, 85%)";
+    }
+    
+}
+
+function changeColorOnBlur(index) {
+    const d = new Date();
+    let day = d.getDay();
+
+    let identifier = "day-" + index;
+
+    if (index == day) {
+        document.getElementById(identifier).style.backgroundColor = "var(--cyan)";
+    } else {
+        document.getElementById(identifier).style.backgroundColor = "var(--soft-red)";
+    }
+}
+
+function displayIncome(index) {
+    let identifier = "income-info-" + index;
+
+    document.getElementById(identifier).style.display = "flex";
+}
+
+function hideIncome(index) {
+    let identifier = "income-info-" + index;
+
+    document.getElementById(identifier).style.display = "none";
+}
